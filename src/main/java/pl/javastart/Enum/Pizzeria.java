ab276fa3e2d342bd7e6fc4c535d11578dd3d56c0
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2022-12-07 10:00:42 +0100

    enum

--- src/main/java/pl/javastart/Enum/Pizzeria.java
+++ src/main/java/pl/javastart/Enum/Pizzeria.java
@@ -1,27 +1,56 @@
 package pl.javastart.Enum;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Scanner;
 
 import static pl.javastart.Enum.Pizza.*;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pizzeria {
-   static String choice;
+    private String choice;
+
+
+
+
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wybierz swoją pizze:\n");
+            System.out.println("Wybierz swoją pizze:\n");
+            for (Pizza pizza: values() ){
+                System.out.println(pizza);}
 
-        for (Pizza pizza: values() ){
-            System.out.println(pizza);
+
+        String p = scanner.nextLine();
+         p=p.toUpperCase();
+
+
+        switch (p){
+            case ("MARGARITHA"):
+            break;
+            case("CAPRICIOSA"):
+
+            break;
+            case("PROSCIUTTIO"):
+            break;
+
+            default:
+                System.out.println("Wybierz swoją pizze:\n");
+                for (Pizza pizza: values() ){
+                    System.out.println(pizza);}
+                p=scanner.nextLine();
+                p=p.toUpperCase();
+                break;
+
         }
-        choice = scanner.nextLine();
-
-
-
-            Pizza pizza = valueOf(choice);
+            Pizza pizza = Pizza.valueOf(p);
 
             System.out.println("Zamówiłeś pizzę : " + pizza.name());
         scanner.close();
