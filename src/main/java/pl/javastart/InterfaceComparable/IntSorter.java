4b74d4ff01ec79e69289e8ac0f20ff8f3c24e1ef
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2023-03-21 14:02:00 +0100

    Tablica sortowanie w malejące przez porównanie

---
+++ src/main/java/pl/javastart/InterfaceComparable/IntSorter.java
@@ -0,0 +1,31 @@
+package pl.javastart.InterfaceComparable;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+public class IntSorter {
+    public static void main(String[] args) {
+        Integer[] numbers = new Integer[1000];
+        Random generator = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i]=generator.nextInt(1000);
+        }
+
+       System.out.println("Wygeneerowana tablica: " + Arrays.toString(numbers));
+
+        Arrays.sort(numbers);
+        System.out.println("Tablica sortowana rosnąco" + Arrays.toString(numbers));
+
+        Arrays.sort(numbers,
+                new Comparator<Integer>(){
+            @Override
+                 public int compare(Integer  o1,  Integer o2) {
+                return o1.compareTo(o2)*(-1);
+            }
+
+        });
+        System.out.println("Tablica sortowana malejąco" + Arrays.toString(numbers) );
+    }
+}
